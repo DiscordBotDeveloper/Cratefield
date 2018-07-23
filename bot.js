dd9d77b11eb7da16d43fa1237d6f17a2269eb2f1
Panda1 <buddabane5@gmail.com>
2018-07-23 13:23:55 -0400

    Maybe a bug fix

--- bot.js
+++ bot.js
@@ -11,15 +11,7 @@ client.on('ready', () => {
 client.on('message', msg => {
     if (msg.content == '!order') {
         msg.reply('What kind of order are you looking to make? Please choose from **plan** or **custom** order.');
-        client.on('message', msg=> {
-            if (msg.content == 'custom') {
-                msg.reply('Please look at #request-a-custom-server.'); // Send in DMs
-            }
-            else if (msg.content == 'plan') {
-                msg.reply('Please look at https://goo.gl/forms/oM3Y62VPhZOwYLTq2.');
-            }
-            break;
-        });
+        msg.reply('*!order plan* or *!order custom*');
     }
 });
 
@@ -27,10 +19,13 @@ client.on('message', msg => {
     if (msg.content == '!order custom') {
         msg.reply('Please look at #request-a-custom-server.'); // Again, send in DMs
     }
-    else if (msg.content == '!order plan') {
-        msg.reply('Please look at https://goo.gl/forms/oM3Y62VPhZOwYLTq2.');
-    }
 });
+
+client.on('message', msg => {
+    if (msg.content == '!order plan') {
+        msg.reply('Please look at http://goo.gl/forms/oM3Y62VPhZOwYLTq2.');
+    }
+})
 
 client.on('message', msg => {
     if (msg.content == '!help') {
