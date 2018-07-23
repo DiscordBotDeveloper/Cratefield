ec8258e354a286715b052db98c4777b253f14416
Panda1 <buddabane5@gmail.com>
2018-07-23 13:18:44 -0400

    Just realized my mistake and fixed it

--- bot.js
+++ bot.js
@@ -5,17 +5,18 @@ client.login(process.env.BOT_TOKEN)
 
 client.on('ready', () => {
     console.log("Bot started. Use !help for a list of commands."); // gonna keep this
+    bot.user.setGame('Glitching...');
 });
 
 client.on('message', msg => {
     if (msg.content == '!order') {
-        msg.reply('What kind of order are you looking to make? Please choose from **plan** or **custom** order.')
+        msg.reply('What kind of order are you looking to make? Please choose from **plan** or **custom** order.');
         client.on('message', msg=> {
             if (msg.content == 'custom') {
-                msg.reply('Please look at #request-a-custom-server.') // Send in DMs
+                msg.reply('Please look at #request-a-custom-server.'); // Send in DMs
             }
             else if (msg.content == 'plan') {
-                msg.reply('Please look at https://goo.gl/forms/oM3Y62VPhZOwYLTq2.')
+                msg.reply('Please look at https://goo.gl/forms/oM3Y62VPhZOwYLTq2.');
             }
             break;
         });
@@ -24,16 +25,16 @@ client.on('message', msg => {
 
 client.on('message', msg => {
     if (msg.content == '!order custom') {
-        msg.reply('Please look at #request-a-custom-server.') // Again, send in DMs
+        msg.reply('Please look at #request-a-custom-server.'); // Again, send in DMs
     }
     else if (msg.content == '!order plan') {
-        msg.reply('Please look at https://goo.gl/forms/oM3Y62VPhZOwYLTq2.')
+        msg.reply('Please look at https://goo.gl/forms/oM3Y62VPhZOwYLTq2.');
     }
-})
+});
 
 client.on('message', msg => {
     if (msg.content == '!help') {
-        msg.reply('Here is a list of commands.\n**!order** - Order a server')
+        msg.reply('Here is a list of commands.\n**!order** - Order a server');
     }
 });
 
