48b6a0b8c56085a0e11ed8767c711b77e15ccdad
Panda1 <buddabane5@gmail.com>
2018-07-23 19:55:55 -0400

    lil change

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN)
 
 client.on('ready', () => {
-    client.user.setGame('Glitching...'); // This doesn't work
+    client.user.setGame('DO NOT USE'); // This doesn't work
 });
 
 client.on('message', msg => {
