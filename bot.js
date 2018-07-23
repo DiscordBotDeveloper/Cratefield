cca3c96738ee7df265d972daa8948557c9f0ecde
Panda1 <buddabane5@gmail.com>
2018-07-23 14:07:54 -0400

    Major rewrite stuff
    thanks dude on discord named duck

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN)
 
 client.on('ready', () => {
-    console.log("Bot started. Use !help for a list of commands."); // gonna keep this
     bot.user.setGame('Glitching...');
 });
 
@@ -12,45 +11,37 @@ client.on('message', msg => {
     if (msg.content == ';order') {
         order = true;
         msg.reply('What kind of order are you looking to make? Please choose from **plan** or **custom** order.');
+        return;
     }
-});
-
-client.on('message', msg => {
-    if (msg.content == 'plan') {
-        if (order) {
-            msg.reply('Please look at #request-a-custom-server.');
-        }
-        if (!order) {
-            console.log('Logging in console to exit a break. Maybe it works, maybe it doesnt.');
-        }
-    }
-});
-
-client.on('message', msg => {
-    if (msg.content == 'custom') {
-        if (order) {
+    else if (msg.content == 'plan') {
+        if (order == true) {
             msg.reply('Please look at http://goo.gl/forms/oM3Y62VPhZOwYLTq2.');
+            order = false;
+            return;
         }
-        if (!order) {
-            console.log('This probably wont work');
-        } 
+        else {
+            order = false; // This happens if another person says something before you can write plan or custom so....... poop
+            return;
+        }
     }
-});
-
-client.on('message', msg => {
-    if (msg.content == ';order custom') {
-        msg.reply('Please look at #request-a-custom-server.'); // Again, send in DMs
+    else if (msg.content == 'custom') {
+        if (order == true) {
+            msg.reply('Please look at #request-a-custom-server.'); // Send in DMs
+            order = false;
+            return;
+        }
+        else {
+            order = false; // Maybe don't have this? idk
+            return;
+        }
     }
-});
-
-client.on('message', msg => {
-    if (msg.content == ';order plan') {
+    else if (msg.content == ';order custom') {
+        msg.reply('Please look at #request-a-custom-server');
+    }
+    else if (msg.content == ';order plan') {
         msg.reply('Please look at http://goo.gl/forms/oM3Y62VPhZOwYLTq2.');
     }
-});
-
-client.on('message', msg => {
-    if (msg.content == ';help') {
-        msg.reply('Here is a list of commands.\n**!order** - Order a server');
+    else if (msg.content == ';help') {
+        msg.reply('Here is a list of commands.\n**;help** - View commands\n**;order** - Order a server');
     }
 });
