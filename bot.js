fa9c06c7462408a59188b70505e3f840bf2e8916
Panda1 <buddabane5@gmail.com>
2018-07-23 21:13:48 -0400

    test

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ client.on('message', msg => {
         return;
     }
     else if (msg.content == ';help') {
-        msg.reply('Here is a list of commands.\n**;help** - View commands\n**;order** - Order a server\n**;readme** - View the readme\n**;dailymeme** - Updated every day with a new meme');
+        msg.reply('Here is a list of commands.\n**;help** - View commands\n**;order** - Order a server\n**;readme** - View the readme\n**;dailymeme** - Updated every day with a new meme\n**;fortnite** - Add the argument version to find the current version, and add the argument advert to advertise your game.');
         return;
     }
     else if (msg.content == ';dailymeme') {
@@ -53,6 +53,18 @@ client.on('message', msg => {
     }
     else if (msg.content == ';readme') {
         msg.reply('README Contents:\n **Cratefield**\n-----------------------\nCratefield is a bot for the server Cratefield Premade\nBuy custom made discord servers today!\n**News:**\nThe bot will flicker offline and online when im updating it.\n~The bot is currently down rn cause~ my github account is "FLAGGED" lol ok but why *(Doesnt shut down the bot, but my discord is now private. $7 per month fee for free!)*');
+        return;
+    }
+    else if (msg.content == ';fortnite') {
+        msg.reply('Please look at ;help for possible arguments');
+        return;
+    }
+    else if (msg.content == ';fortnite version') {
+        msg.reply('Fortnite is current v5.0, and will be updated to v5.1 at 4:00 A.M. EST 7/23/18');
+        return;
+    }
+    else if (msg.content == ';fortnite advert') {
+        msg.reply(`${client.user} is playing fortnite! Message them to play!`);
         return;
     }
     return;
